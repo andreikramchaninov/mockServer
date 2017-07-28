7380f3cea2adc68f975c8105075d7d71a21ae70b
andeikramchaninov <andreikramchaninov@gmail.com>
2017-07-28 21:49:11 +0300

    added approved field to versionData

--- data/versionData.js
+++ data/versionData.js
@@ -1,5 +1,5 @@
 function Version(id, user_id, project_id, version_number, 
-	download_link, approved, commit_message, created_date) {
+	download_link, approved, commit_message, created_date, archived) {
     this.id = id;
     this.user_id = user_id;
     this.project_id = project_id;
@@ -8,12 +8,13 @@ function Version(id, user_id, project_id, version_number,
     this.approved = approved;
     this.commit_message = commit_message;
     this.created_date = created_date;
+    this.archived = archived;
 };
 
-var version0 = new Version(0,0,0,0,"/myfile/test.xml",1, "base project0 version", "01012017");
-var version1 = new Version(1,1,0,1,"/myfile/test.xml",0, "unapproved project version", "01012017");
-var version2 = new Version(2,2,1,0,"/myfile/test.xml",1, "base project1 version", "01012017");
-var version3 = new Version(3,3,1,1,"/myfile/test.xml",0, "unapproved project version", "01012017");
+var version0 = new Version(0,0,0,0,"/myfile/test.xml",1, "base project0 version", "01012017", 0);
+var version1 = new Version(1,1,0,1,"/myfile/test.xml",0, "unapproved project version", "01012017", 0);
+var version2 = new Version(2,2,1,0,"/myfile/test.xml",1, "base project1 version", "01012017", 0);
+var version3 = new Version(3,3,1,1,"/myfile/test.xml",0, "unapproved project version", "01012017", 0);
 
 
 var versionData = [version0, version1, version2, version3];
